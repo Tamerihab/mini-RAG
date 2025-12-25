d5e4e227c01e4665f9a24be5184b0bb4b9a76f4c
Tamerihab <tamerihab02@gmail.com>
2025-12-26 00:20:52 +0200

    Implement async file upload endpoint with validation and error handling

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, Settings
-from controllers import DataController
+from controllers import DataController, ProjectController
+import aiofiles
+from models import ResponseSignal
+import logging
+
+logger = logging.getLogger('uvicorn.error')
 
 data_router = APIRouter(
     prefix="/api/v1/data",
@@ -14,8 +20,40 @@ async def upload_data(
     project_id: str,
     file: UploadFile,
     app_settings: Settings = Depends(get_settings)
-):  
+):
     data_controller = DataController()
-    validation_result = await data_controller.validate_file(file)
-    return validation_result
-    
+    is_valid, result_signal = await data_controller.validate_file(file)
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": result_signal
+            }
+        )
+
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path = data_controller.generate_unqiue_filename(
+        original_filename=file.filename,
+        project_id=project_id
+    )
+    try:
+        async with aiofiles.open(file_path, 'wb') as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+
+        logger.error(f"Error while uploading file: {e}")
+
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_UPLOAD_FAILED.value,
+            }
+        )
+
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={
+            "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value
+        }
+    )
