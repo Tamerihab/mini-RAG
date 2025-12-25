3834f7cdc02f1307ec7f1584de35a8450fedf62b
Tamerihab <tamerihab02@gmail.com>
2025-12-26 00:20:52 +0200

    Add ResponseSignal enum and model export for standardized API responses

--- src/models/enums/ResponseEnum.py
+++ src/models/enums/ResponseEnum.py
@@ -2,5 +2,11 @@ from enum import Enum
 import os
 from helpers.config import get_settings
 
-class ResponseEnum(str, Enum):
-    
+
+class ResponseSignal(Enum):
+
+    FILE_VALIDATED_SUCCESS = "File validated successfully."
+    FILE_TYPE_NOT_SUPPORTED = "File type not supported."
+    FILE_SIZE_EXCEEDED = "File size exceeded."
+    FILE_UPLOAD_SUCCESS = "File uploaded successfully."
+    FILE_UPLOAD_FAILED = "File upload failed."
