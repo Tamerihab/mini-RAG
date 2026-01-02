0f5393328458efea2cce282ce2badd11eef348d6
Tamerihab <tamerihab02@gmail.com>
2026-01-02 21:01:01 +0200

    Add process file endpoint and request schema to data routes

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
-import os
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
+from controllers import DataController, ProjectController, ProcessController
 import aiofiles
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -30,6 +30,9 @@ async def upload_data(
                 "signal": result_signal
             }
         )
+    
+    # there was a problem with the file read pointer as it was at the end of the file after validation so to fix that we need to reset it.
+    await file.seek(0)
 
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
     file_path, file_id = data_controller.generate_unique_filepath(
@@ -55,6 +58,39 @@ async def upload_data(
         status_code=status.HTTP_201_CREATED,
         content={
             "signal": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-            "file_id" : file_id
+            "file_id": file_id
         }
     )
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(
+    project_id: str,
+    request: ProcessRequest,
+):
+    file_id = request.file_id
+    chunk_size = request.chunk_size
+    overlap_size = request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or file_chunks == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.FILE_PROCESSING_FAILED.value,
+            }
+        )
+    
+    return file_chunks
+
+  
