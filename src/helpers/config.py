cd359827af22297d4bc633e1c74dd2a99e79253d
Tamerihab <tamerihab02@gmail.com>
2026-06-05 23:02:11 +0300

    update main and config files

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -9,11 +9,31 @@ class Settings(BaseSettings):
     APP_NAME: str 
     APP_VERSION: str
     OPEN_API_KEY: str
+
     FILE_ALLOWED_EXTENSIONS: list[str]
     FILE_MAX_SIZE_MB: int
     FILE_DEFAULT_CHUNK_SIZE: int
+
     MONGODB_URI: str
     MONGODB_DATABASE: str
+        
+    GENERATION_BACKEND:str
+    EMBEDDING_BACKEND:str
+
+    OPENAI_API_KEY: str= None
+    OPENAI_API_URL: str= None
+    COHERE_API_KEY: str= None
+    DEEPSEEK_API_KEY: str= None
+
+
+    GENERATION_MODEL_ID: str= None 
+    EMBEDDING_MODEL_ID: str= None
+    EMBEDDING_MODEL_SIZE: int= None
+
+    INPUT_DEFAULT_MAX_CHARACTERS: int= None
+    GENERATION_DEFAULT_MAX_TOKENS: int= None
+    GENERATION_DEFAULT_TEMPERATURE: float= None
+
 
 def get_settings() -> Settings:
     return Settings()
